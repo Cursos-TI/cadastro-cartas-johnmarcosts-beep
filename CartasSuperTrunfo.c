f1d46645061daeaeb0cab586678b8a7ad2b53e45
johnmarcosts-beep <johnmarcosts@gmail.com>
2026-07-27 21:06:19 -0300

    Implement city data input and output
    Added input and output for city properties including name, PIB, and population.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,26 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+int cidade1[100];
+int pib1;
+int populacao1;
 
-  // Área para entrada de dados
+    printf("Digite o nome da cidade 1: "); 
+    scanf("%s", cidade1);
+
+    printf("Digite o PIB da cidade 1: ");
+    scanf("%d", &pib1);
+
+    printf("Digite a população da cidade 1: ");
+    scanf("%d", &populacao1);
 
   // Área para exibição dos dados da cidade
+  printf("Cidade 1: %s\n", cidade1);
+  printf("PIB da cidade 1: %d\n", pib1);    
+  printf("População da cidade 1: %d\n", populacao1);
+
+  int media = (pib1 / populacao1);
+  printf("Média do PIB por habitante da cidade 1: %d\n", media);
 
 return 0;
 } 
